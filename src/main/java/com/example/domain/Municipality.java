fd3e96a7f4c176a57e0fdc0aaa46b86f569c53d0
takamasa-like-curry <sugaharatakamasa@localhost>
2023-05-23 14:43:36 +0900

    municipalityテーブルにデータを挿入するための実装を行いました。

---
+++ src/main/java/com/example/domain/Municipality.java
@@ -0,0 +1,37 @@
+package com.example.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/**
+ * municipalitiesテーブルの情報を保持するクラス.
+ * 
+ * @author sugaharatakamasa
+ *
+ */
+@Data
+@Entity
+@Table(name = "municipalities")
+public class Municipality {
+
+	/** 市区町村ID */
+	@Id
+	@Column(name = "id")
+	private int id;
+	/** 市区町村名 */
+	@Column(name = "name")
+	private String name;
+	/** 市区町村名カナ */
+	@Column(name = "name_kana")
+	private String nameKana;
+	/** 市区町村名ローマ字 */
+	@Column(name = "name_rome")
+	private String nameRome;
+	/** 都道府県ID */
+	@Column(name = "prefecture_id")
+	private int prefectureId;
+
+}
---
+++ src/main/java/com/example/writer/MunicipalityWriter.java
@@ -0,0 +1,35 @@
+package com.example.writer;
+
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.stereotype.Component;
+
+import com.example.domain.Municipality;
+
+import jakarta.persistence.EntityManagerFactory;
+
+/**
+ * municipalitiesテーブルに挿入を行うクラス.
+ * 
+ * @author sugaharatakamasa
+ *
+ */
+@Component
+@StepScope
+public class MunicipalityWriter implements ItemWriter<Municipality> {
+
+	private JpaItemWriter<Municipality> writer;
+
+	public MunicipalityWriter(EntityManagerFactory entityManagerFactory) {
+		writer = new JpaItemWriter<>();
+		writer.setEntityManagerFactory(entityManagerFactory);
+	}
+
+	@Override
+	public void write(Chunk<? extends Municipality> chunk) throws Exception {
+		writer.write(chunk);
+
+	}
+}
