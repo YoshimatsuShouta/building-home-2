edc5edf373395ecc27c9d215835140238c0dee16
takamasa-like-curry <sugaharatakamasa@localhost>
2023-05-25 16:06:48 +0900

    originalテーブルに情報が挿入されている前提からの各テーブルへの情報挿入及び、ひったくり情報の挿入を行うためのバッチの設定を行いました。

--- src/main/java/com/example/common/BatchConfig.java
+++ src/main/java/com/example/common/BatchConfig.java
@@ -8,18 +8,24 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.example.domain.Address;
+import com.example.domain.Hittakuri;
+import com.example.domain.HittakuriOriginal;
 import com.example.domain.Municipality;
 import com.example.domain.Prefecture;
 import com.example.listner.PrefectureRederListner;
 import com.example.listner.PrefectureStepListner;
+import com.example.processor.HittakuriProcessor;
 import com.example.reader.AddressReader;
+import com.example.reader.HittakuriOriginalReader;
 import com.example.reader.MunicipalityReader;
 import com.example.reader.PrefectureReader;
 import com.example.writer.AddressWriter;
+import com.example.writer.HittakuriWriter;
 import com.example.writer.MunicipalityWriter;
 import com.example.writer.PrefectureWriter;
 
@@ -30,6 +36,7 @@ import com.example.writer.PrefectureWriter;
  *
  */
 @Component
+@Configuration
 public class BatchConfig {
 
 	@Autowired
@@ -46,17 +53,16 @@ public class BatchConfig {
 	private AddressReader addressReader;
 	@Autowired
 	private AddressWriter addressWriter;
-	///////////////// listner/////////////////////
+
 	@Autowired
-	private PrefectureRederListner prefectureRederListner;
+	private HittakuriProcessor hittakuriProcessor;
 	@Autowired
-	private PrefectureStepListner prefectureStepListner;
+	private HittakuriWriter hittakuriWriter;
 
 	@Bean
 	Step stepOfPrefecture(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
 		return new StepBuilder("stepOfPrefecture", jobRepository).<Prefecture, Prefecture>chunk(50, transactionManager)
-				.reader(prefectureReader).writer(prefectureWriter).listener(prefectureRederListner)
-				.listener(prefectureStepListner).build();
+				.reader(prefectureReader).writer(prefectureWriter).build();
 	}
 
 	@Bean
@@ -73,9 +79,35 @@ public class BatchConfig {
 	}
 
 	@Bean
-	Job job(JobRepository jobRepository, Step stepOfPrefecture, Step stepOfMunicipality, Step stepOfAddress) {
+	Step stepOfHittakuriTokyo2021(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+		return new StepBuilder("stepOfHittakuriTokyo2021", jobRepository)
+				.<HittakuriOriginal, Hittakuri>chunk(1000, transactionManager)
+				.reader(new HittakuriOriginalReader("/csv/hittakuri/tokyo_2021hittakuri.csv"))
+				.processor(hittakuriProcessor).writer(hittakuriWriter).build();
+	}
+
+	@Bean
+	Step stepOfHittakuriTokyo2020(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+		return new StepBuilder("stepOfHittakuriTokyo2020", jobRepository)
+				.<HittakuriOriginal, Hittakuri>chunk(1000, transactionManager)
+				.reader(new HittakuriOriginalReader("/csv/hittakuri/tokyo_2020hittakuri.csv"))
+				.processor(hittakuriProcessor).writer(hittakuriWriter).build();
+	}
+
+	@Bean
+	Step stepOfHittakuriTokyo2019(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+		return new StepBuilder("stepOfHittakuriTokyo2019", jobRepository)
+				.<HittakuriOriginal, Hittakuri>chunk(1000, transactionManager)
+				.reader(new HittakuriOriginalReader("/csv/hittakuri/tokyo_2019hittakuri.csv"))
+				.processor(hittakuriProcessor).writer(hittakuriWriter).build();
+	}
+
+	@Bean
+	Job job(JobRepository jobRepository, Step stepOfPrefecture, Step stepOfMunicipality, Step stepOfAddress,
+			Step stepOfHittakuriTokyo2021, Step stepOfHittakuriTokyo2020, Step stepOfHittakuriTokyo2019) {
 		return new JobBuilder("job", jobRepository).incrementer(new RunIdIncrementer()).start(stepOfPrefecture)
-				.next(stepOfMunicipality).next(stepOfAddress).build();
+				.next(stepOfMunicipality).next(stepOfAddress).next(stepOfHittakuriTokyo2021)
+				.next(stepOfHittakuriTokyo2020).next(stepOfHittakuriTokyo2019).build();
 	}
 
 }
