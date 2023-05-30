b2942a9707c76f283ef1ad2170943b56ca337e56
takamasa-like-curry <sugaharatakamasa@localhost>
2023-05-30 10:22:39 +0900

    town,choumeに関しての設定を追加しました。

--- src/main/java/com/example/common/BatchConfig.java
+++ src/main/java/com/example/common/BatchConfig.java
@@ -13,21 +13,27 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.example.domain.Address;
+import com.example.domain.Choume;
 import com.example.domain.Hittakuri;
 import com.example.domain.HittakuriOriginal;
 import com.example.domain.Municipality;
 import com.example.domain.Prefecture;
-import com.example.listner.PrefectureRederListner;
-import com.example.listner.PrefectureStepListner;
+import com.example.domain.Town;
+import com.example.processor.ChoumeProcessor;
 import com.example.processor.HittakuriProcessor;
+import com.example.processor.PrefectureProcessor;
+import com.example.processor.TownProcessor;
 import com.example.reader.AddressReader;
+import com.example.reader.AddressReaderForCreateTown;
 import com.example.reader.HittakuriOriginalReader;
 import com.example.reader.MunicipalityReader;
 import com.example.reader.PrefectureReader;
 import com.example.writer.AddressWriter;
+import com.example.writer.ChoumeWriter;
 import com.example.writer.HittakuriWriter;
 import com.example.writer.MunicipalityWriter;
 import com.example.writer.PrefectureWriter;
+import com.example.writer.TownWriter;
 
 /**
  * バッチ処理の設定を行うクラス.
@@ -41,6 +47,8 @@ public class BatchConfig {
 
 	@Autowired
 	private PrefectureReader prefectureReader;
+	@Autowired
+	private PrefectureProcessor prefectureProcessor;
 	@Autowired
 	private PrefectureWriter prefectureWriter;
 
@@ -59,10 +67,22 @@ public class BatchConfig {
 	@Autowired
 	private HittakuriWriter hittakuriWriter;
 
+	@Autowired
+	private AddressReaderForCreateTown addressReaderForCreateTown;
+	@Autowired
+	private TownProcessor townProcessor;
+	@Autowired
+	private TownWriter townWriter;
+
+	@Autowired
+	private ChoumeProcessor choumeProcessor;
+	@Autowired
+	private ChoumeWriter choumeWriter;
+
 	@Bean
 	Step stepOfPrefecture(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
 		return new StepBuilder("stepOfPrefecture", jobRepository).<Prefecture, Prefecture>chunk(50, transactionManager)
-				.reader(prefectureReader).writer(prefectureWriter).build();
+				.reader(prefectureReader).processor(prefectureProcessor).writer(prefectureWriter).build();
 	}
 
 	@Bean
@@ -103,11 +123,25 @@ public class BatchConfig {
 	}
 
 	@Bean
+	Step stepOfTown(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+		return new StepBuilder("stepOfTown", jobRepository).<Address, Town>chunk(1000, transactionManager)
+				.reader(addressReaderForCreateTown).processor(townProcessor).writer(townWriter).build();
+	}
+
+	@Bean
+	Step stepOfChoume(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+		return new StepBuilder("stepOfChoume", jobRepository).<Address, Choume>chunk(1000, transactionManager)
+				.reader(addressReaderForCreateTown).processor(choumeProcessor).writer(choumeWriter).build();
+	}
+
+	@Bean
 	Job job(JobRepository jobRepository, Step stepOfPrefecture, Step stepOfMunicipality, Step stepOfAddress,
-			Step stepOfHittakuriTokyo2021, Step stepOfHittakuriTokyo2020, Step stepOfHittakuriTokyo2019) {
+			Step stepOfHittakuriTokyo2021, Step stepOfHittakuriTokyo2020, Step stepOfHittakuriTokyo2019,
+			Step stepOfTown, Step stepOfChoume) {
 		return new JobBuilder("job", jobRepository).incrementer(new RunIdIncrementer()).start(stepOfPrefecture)
 				.next(stepOfMunicipality).next(stepOfAddress).next(stepOfHittakuriTokyo2021)
-				.next(stepOfHittakuriTokyo2020).next(stepOfHittakuriTokyo2019).build();
+				.next(stepOfHittakuriTokyo2020).next(stepOfHittakuriTokyo2019).next(stepOfTown).next(stepOfChoume)
+				.build();
 	}
 
 }
