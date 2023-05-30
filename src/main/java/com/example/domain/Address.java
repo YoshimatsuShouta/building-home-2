46acac9f80fc6355414d079cdef06f1822ea70d6
takamasa-like-curry <sugaharatakamasa@localhost>
2023-05-30 10:20:50 +0900

    改行の改修

--- src/main/java/com/example/domain/Address.java
+++ src/main/java/com/example/domain/Address.java
@@ -24,26 +24,34 @@ public class Address {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private int id;
+
 	/** 町丁目名 */
 	@Column(name = "name")
 	private String name;
+
 	/** 町丁目名カナ */
 	@Column(name = "name_kana")
 	private String nameKana;
+
 	/** 町丁目名ローマ字 */
 	@Column(name = "name_rome")
 	private String nameRome;
+
 	/** 緯度 */
 	@Column(name = "latitude")
 	private Double latitude;
+
 	/** 経度 */
 	@Column(name = "longitude")
 	private Double longitude;
+
 	/* 都道府県ID */
 	@Column(name = "prefecture_id")
 	private int prefectureId;
+
 	/** 市区町村ID */
 	@Column(name = "municipality_id")
 	private int municipalityId;
+
 
 }
